3d91699c805412bf457b0d77409e8eed4d5822f8
Crax97 <gsolimeno97@gmail.com>
2023-09-06 12:44:38 +0200

    Automatically install fonts
    When fonts are already installed, pass --force-install-fonts to
    force installing fonts. By default Iosevka is installed

--- install.sh
+++ install.sh
@@ -1,25 +1,36 @@
 #! /bin/bash
 
 install_font () {
-	echo Installing Iosevka font...;
-	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Iosevka.zip;
+	FONT_TO_INSTALL=$1;
+	if [[ $FONT_TO_INSTALL == "" ]]
+	then
+		FONT_TO_INSTALL = "Iosevka"
+	fi
+	echo Installing $FONT_TO_INSTALL font...;
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/$FONT_TO_INSTALL.zip;
 	mkdir -p $HOME/.local/share/fonts;
 	mkdir font;
 	cd font;
-	unzip ../Iosevka.zip;
+	unzip ../$FONT_TO_INSTALL.zip;
 	mv ./* $HOME/.local/share/fonts/;
 	fc-cache -v;
 	cd ..;
 	
-	echo "Clearing Iosevka install files";
+	echo "Clearing $FONT_TO_INSTALL install files";
 	rm -rf font;
-	rm Iosevka.zip
-
-
+	rm $FONT_TO_INSTALL.zip
 }
 
+FONT_TO_INSTALL="Iosevka"
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 ln -s -T $ROOT_DIR/nvim $HOME/.config/nvim
+ln -s -T $ROOT_DIR/alacritty $HOME/.config/alacritty
 
-install_font
+FOUND_FONTS=$(find $HOME/.local/share/fonts -iname "$FONT_TO_INSTALL*.ttf")
+if [[ "${#FOUND_FONTS}" == "0" || $@ == *"--force-install-fonts"* ]]
+then
+	install_font $FONT_TO_INSTALL
+else
+	echo Avoid installing fonts
+fi
